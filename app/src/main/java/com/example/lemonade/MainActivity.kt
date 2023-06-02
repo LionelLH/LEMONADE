436f94919bea9a7ee233f38b40ae6b89e23284ca
LHOTTE <l.lhotte@thiriez-literie.fr>
2023-06-02 16:20:39 +0200

    STARTER PACK LEMONAPP

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LEMONADETheme
 
@@ -20,24 +27,30 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                    //color = MaterialTheme.colors.background
+                ){
+                    LemonApp()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LemonApp() {
+    Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        Button(onClick = { /*TODO*/ }) {
+            Image(painter = painterResource(R.drawable.lemon_tree), contentDescription = "")
+            LemonAppProcess( stringResource(R.string.lemon))
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview(){
-    LEMONADETheme {
-        Greeting("Android")
-    }
+fun LemonAppProcess(name: String,modifier : Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
+) {
+    Text(text = "Hello! $name")
 }
